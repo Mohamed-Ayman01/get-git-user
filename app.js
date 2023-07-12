276bd50a35658393037bde3af5d64d0542d8d8f8
Mohamed-Ayman01 <lord39597@gmail.com>
2023-07-13 00:44:23 +0400

    update: wrong user error fixed, error animation made

--- app.js
+++ app.js
@@ -2,80 +2,102 @@ let form = document.forms[0];
 let nameInput = document.querySelector("form #username");
 let submitBtn = document.querySelector("form #submit");
 
+let resBox = document.querySelector(".result-box");
 let userInfoCont = document.querySelector(".info-container");
-let ProjectsCont = document.querySelector(".projects");
+let projectsCont = document.querySelector(".projects");
 
 async function getDataOf(user) {
   userInfoCont.innerHTML = "";
-  ProjectsCont.innerHTML = "";
+  projectsCont.innerHTML = "";
 
+  // ! Data Fetching
   let userData = await fetch(`https://api.github.com/users/${user}`);
-  let userResponse = await userData.json();
-
-  // ! Fetching User Bio
-  let avatarImg = document.createElement("img");
-  avatarImg.className = "avatar";
-  avatarImg.src = userResponse.avatar_url;
-
-  let textBox = document.createElement("div");
-  textBox.className = "text-box";
-
-  let username = document.createElement("h3");
-  username.className = "name";
-  username.innerHTML = `<a href="${userResponse.html_url}" target="_blank"><i class="fas fa-link"></i></a> ${userResponse.name}`;
-  let userBio = document.createElement("p");
-  userBio.className = "bio";
-  userBio.textContent = userResponse.bio;
-
-  let stats = document.createElement("div");
-  stats.classList.add("stats");
-
-  let followers = document.createElement("div");
-  followers.className = "followers";
-  followers.innerHTML = `<span>${userResponse.followers}</span> <p>followers</p>`;
-  let following = document.createElement("div");
-  following.className = "following";
-  following.innerHTML = `<span>${userResponse.following}</span> <p>following</p>`;
-
-  stats.append(followers, following);
-
-  textBox.append(username, userBio, stats);
-
-  userInfoCont.append(avatarImg, textBox);
-
-  // ! Fetching User Projects
   let projectsData = await fetch(`https://api.github.com/users/${user}/repos`);
-  let projectsResponse = await projectsData.json();
 
-  projectsResponse.forEach(function (element) {
-    if (element.fork === false) {
-      let item = document.createElement("div");
-      item.classList.add("item");
+  if (userData.ok === true && projectsData.ok === true) {
+    resBox.classList.remove("hidden");
 
-      let projText = document.createElement("div");
+    // ! Extracting User Data
+    let userResponse = await userData.json();
 
-      let name = document.createElement("h3");
-      name.classList.add("name");
-      name.textContent = `${element.name}`;
+    let avatarImg = document.createElement("img");
+    avatarImg.className = "avatar";
+    avatarImg.src = userResponse.avatar_url;
 
-      let desc = document.createElement("p");
-      desc.classList.add("desc");
-      desc.textContent = `${element.description ?? "No Description"}`;
+    let textBox = document.createElement("div");
+    textBox.className = "text-box";
 
-      projText.append(name, desc);
+    let username = document.createElement("h3");
+    username.className = "name";
+    username.innerHTML = `<a href="${userResponse.html_url}" target="_blank"><i class="fas fa-link"></i></a> ${userResponse.name}`;
+    let userBio = document.createElement("p");
+    userBio.className = "bio";
+    userBio.textContent = userResponse.bio;
 
-      let link = document.createElement("a");
-      link.textContent = "view repo";
-      link.href = `${element.html_url}`;
-      link.target = "_blank";
+    let stats = document.createElement("div");
+    stats.classList.add("stats");
 
-      item.append(projText, link);
-      ProjectsCont.append(item);
+    let followers = document.createElement("div");
+    followers.className = "followers";
+    followers.innerHTML = `<span>${userResponse.followers}</span> <p>followers</p>`;
+    let following = document.createElement("div");
+    following.className = "following";
+    following.innerHTML = `<span>${userResponse.following}</span> <p>following</p>`;
+
+    stats.append(followers, following);
+
+    textBox.append(username, userBio, stats);
+
+    userInfoCont.append(avatarImg, textBox);
+
+    // ! Extracting User Projects
+    let projectsResponse = await projectsData.json();
+
+    projectsResponse.forEach(function (element) {
+      if (element.fork === false) {
+        let item = document.createElement("div");
+        item.classList.add("item");
+
+        let projText = document.createElement("div");
+
+        let name = document.createElement("h3");
+        name.classList.add("name");
+        name.textContent = `${element.name}`;
+
+        let desc = document.createElement("p");
+        desc.classList.add("desc");
+        desc.textContent = `${element.description ?? "No Description"}`;
+
+        projText.append(name, desc);
+
+        let link = document.createElement("a");
+        link.textContent = "view repo";
+        link.href = `${element.html_url}`;
+        link.target = "_blank";
+
+        item.append(projText, link);
+        projectsCont.append(item);
+      }
+    });
+
+    // console.log(userData);
+    // console.log(projectsData);
+  } else {
+    nameInput.classList.add("wrong-anim");
+
+    if (document.querySelector(".warn") == null) {
+      let warnBox = document.createElement("div");
+      warnBox.className = "warn";
+      warnBox.textContent = "invalid user name";
+      warnBox.style = `padding: 1rem; color: red; background-color: var(--dark-black); position: absolute; top: -60px; left: 50%; transform: translateX(-50%); text-transform: capitalize;`;
+      document.body.append(warnBox);
     }
-  });
 
-  console.log(userResponse);
-  console.log(projectsResponse);
+    setTimeout(() => {
+      document.querySelector(".warn").remove();
+      nameInput.classList.remove("wrong-anim");
+    }, 3000);
+  }
 }
 
 form.addEventListener("submit", (e) => {
